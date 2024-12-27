e3f0931a57469219c5c5425fd203b8b199f69167
Dave Cadelina <dave.cadelina@reckon.com>
2024-12-28 00:50:26 +1100

    starting again

--- App.tsx
+++ App.tsx
@@ -1,23 +1,23 @@
 import { StyleSheet } from "react-native";
 import { StatusBar } from "expo-status-bar";
 import HomeBackground from "./components/HomeBackground";
-import WeatherTabBar from "./components/tabbar/WeatherTabBar";
+import React from "react";
 
 export default function App() {
-	return (
-		<>
-			<HomeBackground />
-			<WeatherTabBar />
-			<StatusBar style="light" />
-		</>
-	);
+  return (
+    <>
+      <HomeBackground />
+      {/* <WeatherTabBar /> */}
+      <StatusBar style="light" />
+    </>
+  );
 }
 
 const styles = StyleSheet.create({
-	container: {
-		flex: 1,
-		backgroundColor: "#fff",
-		alignItems: "center",
-		justifyContent: "center",
-	},
+  container: {
+    flex: 1,
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+  },
 });
--- components/HomeBackground.tsx
+++ components/HomeBackground.tsx
@@ -1,73 +1,14 @@
-import {
-	Image,
-	ImageBackground,
-	StyleSheet,
-	ScaledSize,
-	View,
-} from "react-native";
-import { Canvas, LinearGradient, Rect, vec } from "@shopify/react-native-skia";
+import { StyleSheet, ScaledSize, View, Text } from "react-native";
 import React from "react";
-import useAppDimensions from "../hooks/useAppDimensions";
 
-// Notes:
-// * This will give a linear gradient background with color using skia
 const HomeBackground = () => {
-	const dimensions = useAppDimensions();
-	const { width, height } = dimensions;
-	const myStyles = styles(dimensions);
-	const smokeHeight = height * 0.6;
-	const smokeOffsetY = height * 0.4;
-
-	return (
-		<View style={{ ...StyleSheet.absoluteFillObject }}>
-			<Canvas style={{ flex: 1 }}>
-				<Rect x={0} y={0} width={width} height={height}>
-					<LinearGradient
-						start={vec(0, 0)}
-						end={vec(width, height)}
-						colors={["#2E335A", "#1C1B33"]}
-					/>
-				</Rect>
-			</Canvas>
-			<ImageBackground
-				source={require("../assets/home/Background.png")}
-				resizeMode={"cover"}
-				style={{ height: "100%" }}
-			>
-				<Canvas
-					style={{
-						height: smokeHeight,
-						...StyleSheet.absoluteFillObject,
-						top: smokeOffsetY,
-					}}
-				>
-					<Rect x={0} y={0} width={width} height={smokeHeight}>
-						<LinearGradient
-							start={vec(width / 2, 0)}
-							end={vec(width / 2, smokeHeight)}
-							colors={["rgba(58,63,84,0)", "rgba(58,63,84,1)"]}
-							positions={[-0.02, 0.54]}
-						/>
-					</Rect>
-				</Canvas>
-				<Image
-					source={require("../assets/home/House.png")}
-					resizeMode={"cover"}
-					style={myStyles.houseImage}
-				/>
-			</ImageBackground>
-		</View>
-	);
+  return (
+    <View>
+      <Text>HOME BACKGROUND</Text>
+    </View>
+  );
 };
 
 export default HomeBackground;
 
-const styles = ({ width }: ScaledSize) =>
-	StyleSheet.create({
-		houseImage: {
-			width: width,
-			height: width,
-			...StyleSheet.absoluteFillObject,
-			top: "36%",
-		},
-	});
+const styles = ({ width }: ScaledSize) => StyleSheet.create({});
