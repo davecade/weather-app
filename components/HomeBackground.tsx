52ba5afdc00ab66b0421b3b1d07b92cdcaf3e97a
Dave Cadelina <dave.cadelina@reckon.com>
2024-12-28 00:58:56 +1100

    added linear gradient

--- components/HomeBackground.tsx
+++ components/HomeBackground.tsx
@@ -1,11 +1,26 @@
-import { StyleSheet, ScaledSize, View, Text } from "react-native";
+import {
+  StyleSheet,
+  ScaledSize,
+  View,
+  Text,
+  useWindowDimensions,
+} from "react-native";
 import React from "react";
+import { Canvas, LinearGradient, Rect, vec } from "@shopify/react-native-skia";
 
 const HomeBackground = () => {
+  const { width, height } = useWindowDimensions();
+
   return (
-    <View>
-      <Text>HOME BACKGROUND</Text>
-    </View>
+    <Canvas style={{ flex: 1 }}>
+      <Rect x={0} y={0} width={width} height={height}>
+        <LinearGradient
+          start={vec(0, 0)}
+          end={vec(width, height)}
+          colors={["#2E33A5", "#1C1B33"]}
+        />
+      </Rect>
+    </Canvas>
   );
 };
 
