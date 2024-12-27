27647c6130f381a31c83faa863024ebff2a2ab28
Dave Cadelina <dave.cadelina@reckon.com>
2024-12-28 02:02:30 +1100

    added background

--- components/HomeBackground.tsx
+++ components/HomeBackground.tsx
@@ -1,9 +1,8 @@
 import {
   StyleSheet,
   ScaledSize,
-  View,
-  Text,
   useWindowDimensions,
+  ImageBackground,
 } from "react-native";
 import React from "react";
 import { Canvas, LinearGradient, Rect, vec } from "@shopify/react-native-skia";
@@ -12,15 +11,22 @@ const HomeBackground = () => {
   const { width, height } = useWindowDimensions();
 
   return (
-    <Canvas style={{ flex: 1 }}>
-      <Rect x={0} y={0} width={width} height={height}>
-        <LinearGradient
-          start={vec(0, 0)}
-          end={vec(width, height)}
-          colors={["#2E33A5", "#1C1B33"]}
-        />
-      </Rect>
-    </Canvas>
+    <>
+      <Canvas style={{ flex: 1 }}>
+        <Rect x={0} y={0} width={width} height={height}>
+          <LinearGradient
+            start={vec(0, 0)}
+            end={vec(width, height)}
+            colors={["#2E33A5", "#1C1B33"]}
+          />
+        </Rect>
+      </Canvas>
+      <ImageBackground
+        source={require("../assets/home/Background.png")}
+        resizeMode="cover"
+        style={{ height: "100%" }}
+      />
+    </>
   );
 };
 
