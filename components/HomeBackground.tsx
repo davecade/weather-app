4187ffa06e3f3ca27090d59685c8be96dcd2855e
Dave Cadelina <dave.cadelina@reckon.com>
2024-12-29 00:05:12 +1100

    smoke effect

--- components/HomeBackground.tsx
+++ components/HomeBackground.tsx
@@ -6,13 +6,21 @@ import {
   Image,
 } from "react-native";
 import React from "react";
-import { Canvas, LinearGradient, Rect, vec } from "@shopify/react-native-skia";
+import {
+  Canvas,
+  Line,
+  LinearGradient,
+  Rect,
+  vec,
+} from "@shopify/react-native-skia";
 
 const HomeBackground = () => {
   const dimensions = useWindowDimensions();
   const { width, height } = dimensions;
   const myStyles = styles(dimensions);
 
+  const smokeHeight = height * 0.6;
+  const smokeOffsetY = height * 0.4;
   return (
     <>
       <Canvas style={{ flex: 1 }}>
@@ -29,6 +37,22 @@ const HomeBackground = () => {
         resizeMode="cover"
         style={{ height: "100%" }}
       >
+        <Canvas
+          style={{
+            height: smokeHeight,
+            ...StyleSheet.absoluteFillObject,
+            top: smokeOffsetY,
+          }}
+        >
+          <Rect x={0} y={0} width={width} height={smokeHeight}>
+            <LinearGradient
+              start={vec(width / 2)}
+              end={vec(width / 2, smokeHeight)}
+              colors={["rgba(58,63,84,0)", "rgba(58,63,84,1)"]}
+              positions={[-0.02, 0.54]}
+            />
+          </Rect>
+        </Canvas>
         <Image
           source={require("../assets/home/House.png")}
           resizeMode="cover"
--- hooks/useAppDimensions.tsx
+++ hooks/useAppDimensions.tsx
@@ -1,13 +1,17 @@
-import { useWindowDimensions, StatusBar } from "react-native";
+import { useWindowDimensions, StatusBar, ScaledSize } from "react-native";
 
-const useAppDimensions = () => {
-	const { width, height, scale, fontScale } = useWindowDimensions();
-	return {
-		width,
-		height: height + (StatusBar?.currentHeight || 0),
-		scale,
-		fontScale,
-	};
+// this is for Android
+const useAppDimensions = (): ScaledSize => {
+  const { width, height, scale, fontScale } = useWindowDimensions();
+
+  // The height is different on Android, so we need to add the StatusBar height
+  // if its undefined, we just return 0 because it's not an Android device
+  return {
+    width,
+    height: height + (StatusBar?.currentHeight || 0),
+    scale,
+    fontScale,
+  };
 };
 
 export default useAppDimensions;
